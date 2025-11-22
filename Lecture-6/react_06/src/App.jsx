f370aac5057e42ca04575f98a3972d00d9f7392d
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-11-22 12:11:24 +0530

    lecture 5 codes

---
+++ Lecture-6/react_06/src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <Counterr />
+    </div>
+  )
+}
+export default App
+
+
+function Counterr() {
+  const [count,setCount] = useState(0);
+  useEffect(()=>{
+    if(count === 0){
+      console.log("componentDidMount()");
+    }
+    else if(count>=1 && count<=5){
+      console.log("componentDidUpdate()");
+    }
+    // cleanup fn
+    if(count === 6){
+      return ()=>{ console.log("componentWillUnmount") }
+    }
+  }, [count])
+
+  return (
+    <div>
+      <h1>Hello from - {count}</h1>
+      <button onClick={()=>setCount(count+1)} >Click</button>
+    </div>
+  )
+}
+
+
+
+
+// -------- CLASS BASED COMPONENTS ----------
+// import React from 'react'
+// import Counter from './components/Counter';
+
+// class App extends React.Component{
+//   constructor(){
+//     super()
+//     this.state = {
+//       count: 0,
+//     }
+//   }
+//   decrease() {
+//     this.setState({count: this.state.count - 1})
+//   }
+//   componentDidMount(){
+//     console.log("hello");
+//   }
+
+//   componentWillUnmount(){
+//     console.log("component is removed");
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <button onClick={()=>this.setState({count: this.state.count+1})}>Increment</button>
+//         {/* <h1>{this.state.count}</h1> */}
+//         <Counter count={this.state.count} />
+//         <button onClick={this.decrease.bind(this)} >Decrement</button>
+//       </div>
+//     )
+//   }
+// }
+// export default App
---
+++ Lecture-6/react_06/src/components/Counter.jsx
@@ -0,0 +1,21 @@
+
+
+// import React from "react";
+// class Counter extends React.Component{
+
+//     componentDidUpdate(prevProp,prevState){
+//         if(this.props.count !== prevProp.count){
+//             console.log("hi i am updating myself");
+//         }
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <h2> {this.props.count} </h2>
+//             </div>
+//         )
+//     }
+
+// }
+// export default Counter;
