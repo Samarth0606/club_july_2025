4291f826b6219056f48ecc178fee1448834977d3
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-12-12 19:44:46 +0530

    lecture swiggy 1 code

---
+++ Lecture-9/swiggy_01/src/components/Body.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import RestaurantCard from './RestaurantCard'
+
+function Body() {
+  return (
+    <div>
+        <h1 className='font-bold text-2xl m-8'>Restaurants with online food delivery in Chhindwara</h1>
+        <button className='border rounded-2xl ml-20 w-1/12 p-2 text-xl'>Rating 4.4+</button>
+        <button className='border rounded-2xl ml-2 w-1/12 p-2 text-xl'>Reset</button>
+    
+        <div className='flex flex-wrap w-10/12 mx-auto'>
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+        </div>
+    </div>
+  )
+}
+
+export default Body
